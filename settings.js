b7b093f66551c1dd3b14dd98c6460ebfb8014386
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-02-05 18:48:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,8 +29,8 @@ global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6289522724388","6289522724388","6289522724388"] //ganti agar fitur owner bisa di gunakan
-global.packname = '©ImanBot' //sticker wm
-global.author = 'Di Buat Oleh ImanOffc' //sticker wm
+global.packname = '© ImanBot' //sticker wm
+global.author = 'ImanOffc' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
